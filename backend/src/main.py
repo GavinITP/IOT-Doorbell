d4985c10d40184526802b375ce0a9f560635675a
Thammarit Phiwton <poomz180@gmail.com>
2024-10-05 11:54:06 +0700

    use port 8000 for api backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -111,7 +111,7 @@ async def main():
             await asyncio.Future()
 
     async def start_fastapi_server():
-        config = uvicorn.Config("main:app", host="0.0.0.0", port=8080, log_level="info")
+        config = uvicorn.Config("main:app", host="0.0.0.0", port=8000, log_level="info")
         server = uvicorn.Server(config)
         await server.serve()
 
